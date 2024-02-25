d5a426c857178bf88e6a88085370b8bae1430fd5
Kamil Pyszniak <k.pyszniak27@gmail.com>
2024-02-25 10:40:58 +0100

    initial configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>Naglowek H1 ABC</h1>
+      <h2>Naglowek H2 ABC</h2>
+      <h3>Naglowek H3 ABC</h3>
+      <p>PARAGRAF ABC</p>
+      <div></div>
     </>
   )
 }
